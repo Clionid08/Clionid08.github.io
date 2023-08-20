229b018fe9a93a9e7226e59aa5383c52c5a600c3
Clionid08 <namuhaja@naver.com>
2023-08-20 23:32:43 +0900

    edit Title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,9 +64,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Clionid08',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Clionid08',
           src: 'img/logo.svg',
         },
         items: [
